6f903e6d07e0928d64ea271f09656492de6b858a
chris <christopher.poncelet51@gmail.com>
2021-04-17 23:32:14 +0200

    first commit

---
+++ ecommerce_front/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper">
+    <nav class="navbar is-info">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item">Home</router-link>
+
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu" id="navbar-menu">
+        <div class="navbar-end">
+          <router-link to="/summer" class="navbar-item">Summer</router-link>
+          <router-link to="/winter" class="navbar-item">Winter</router-link>
+
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/login" class="button is-light">Login</router-link>
+              <router-link to="/cart" class="button is-success">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span>Panier</span>
+              </router-link>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <section class="section">
+      <router-view/>
+    </section>
+
+    <footer class="footer">
+      <p class="has-text-centered">Copyright © 2021</p>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../node_modules/bulma';
+</style>
