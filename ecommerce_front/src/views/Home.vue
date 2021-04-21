f9f0d64a47d906eecf5e6eddf571b2548a7897c0
chris <christopher.poncelet51@gmail.com>
2021-04-21 23:38:03 +0200

    home view

--- ecommerce_front/src/views/Home.vue
+++ ecommerce_front/src/views/Home.vue
@@ -1,6 +1,40 @@
 <template>
   <div class="home">
-    Home
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          BIENVENUE
+        </p>
+        <p class="subtitle">
+          La marque au design unique
+        </p>
+      </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">
+          Les derniers produits
+        </h2>
+      </div>
+      <div
+          class="column is-3"
+          v-for="product in latestProducts"
+          v-bind:key="product.id"
+      >
+        <div class="box">
+          <figure class="image mb-4">
+            <img v-bind:src="product.get_thumbbail" alt="image-produit">
+          </figure>
+
+          <h3 class="is-size-4">{{ product.name }}</h3>
+          <p class="is-size-6 has-text-centered">{{ product.price}}€</p>
+
+          Voir détails
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
