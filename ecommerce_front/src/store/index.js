51103e2df6e5c14018377ec7d13561025bf0f3b8
chris <christopher.poncelet51@gmail.com>
2021-04-27 01:50:59 +0200

    create function to add to cart

--- ecommerce_front/src/store/index.js
+++ ecommerce_front/src/store/index.js
@@ -10,6 +10,25 @@ export default createStore({
     isLoading: false,
   },
   mutations: {
+    initializeStore(state) {
+      if (localStorage.getItem('cart')) {
+        state.cart = JSON.parse(localStorage.getItem('cart'))
+      } else {
+        localStorage.setItem('cart', JSON.stringify(state.cart))
+      }
+    },
+    /* Ajout au panier */
+    addToCart(state, item) {
+      const exists = state.cart.items.filter(i => i.product.id === item.product.id)
+
+      if (exists.length) {
+        exists[0].quantity = parseInt(exists[0].quantity) + parseInt(item.quantity)
+      } else {
+        state.cart.items.push(item)
+      }
+
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+    }
   },
   actions: {
   },
