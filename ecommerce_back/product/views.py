014f2a0d975a6506c613b9ff8a9895873601157d
chris <christopher.poncelet51@gmail.com>
2021-04-29 23:25:02 +0200

    add search product in back

--- ecommerce_back/product/views.py
+++ ecommerce_back/product/views.py
@@ -1,8 +1,10 @@
 from django.http import Http404
 from django.shortcuts import render
+from django.db.models import Q
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 from .models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
@@ -45,3 +47,17 @@ class CategoryDetail(APIView):
         serializer = CategorySerializer(category)
 
         return Response(serializer.data)
+
+
+@api_view(['POST'])
+def search(request):
+    query = request.data.get('query', '')
+
+    if query:
+        products = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+        serializer = ProductSerializer(products, many=True)
+
+        return Response(serializer.data)
+
+    else:
+        return Response({'products': []})
