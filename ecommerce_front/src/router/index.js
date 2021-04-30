9171a6601b2adecbaa1ec18c413413fb924cd253
chris <christopher.poncelet51@gmail.com>
2021-04-30 13:58:06 +0200

    add search result search

--- ecommerce_front/src/router/index.js
+++ ecommerce_front/src/router/index.js
@@ -1,38 +1,44 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../views/Home.vue'
 
 import Product from "@/views/Product";
 import Category from "@/views/Category";
+import Search from "@/views/Search";
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/:category_slug/:product_slug',
-    name: 'Product',
-    component: Product
-  },
-  {
-    path: '/:category_slug',
-    name: 'Category',
-    component: Category
-  },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/search',
+        name: 'Search',
+        component: Search,
+    },
+    {
+        path: '/:category_slug/:product_slug',
+        name: 'Product',
+        component: Product
+    },
+    {
+        path: '/:category_slug',
+        name: 'Category',
+        component: Category
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
---
+++ ecommerce_front/src/views/Search.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="page-search">
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h1 class="title">
+          Recherche
+        </h1>
+
+        <h2 class="is-size-5 has-text-grey">
+          Terme de recherche : "{{ query }}"
+        </h2>
+      </div>
+
+      <ProductCard
+          v-for="product in products"
+          v-bind:key="product.id"
+          v-bind:product="product"
+      />
+
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import ProductCard from "@/components/ProductCard";
+
+export default {
+  name: "Search",
+  components: {ProductCard},
+  data() {
+    return {
+      products: [],
+      query: ''
+    }
+  },
+
+  mounted() {
+    document.title = 'Recherche | Eshop'
+
+    let uri = window.location.search.substring(1)
+    let params = new URLSearchParams(uri)
+
+    if (params.get('query')) {
+      this.query = params.get('query')
+
+      this.performSearch()
+    }
+  },
+
+  methods: {
+    async performSearch() {
+      this.$store.commit('setIsLoading', true)
+
+      await axios
+          .post('/api/v1/products/search/', {'query': this.query})
+          .then(response => {
+            this.products = response.data
+          })
+          .catch(error => {
+            console.log(error)
+          })
+
+      this.$store.commit('setIsLoading', false)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
