109278662f2a8f402aca0d958870878d1cddd7b8
chris <christopher.poncelet51@gmail.com>
2021-05-01 22:13:15 +0200

    add increment decrement remove item in cart

--- ecommerce_front/src/components/CartItem.vue
+++ ecommerce_front/src/components/CartItem.vue
@@ -5,9 +5,13 @@
       <router-link :to="item.product.get_absolute_url">{{ item.product.name }}</router-link>
     </td>
     <td>{{ item.product.price }}€</td>
-    <td>{{ item.quantity }}</td>
+    <td>
+      {{ item.quantity }}
+      <a @click="decrementQuantity(item)">-</a>
+      <a @click="incrementQuantity(item)">+</a>
+    </td>
     <td>{{ getItemTotal(item).toFixed(2) }}€</td>
-    <td><button class="delete"></button></td>
+    <td><button class="delete" @click="removeFromCart(item)"></button></td>
   </tr>
 
 </template>
@@ -29,6 +33,32 @@ export default {
     getItemTotal(item) {
       return item.quantity * item.product.price
     },
+
+    decrementQuantity(item) {
+      item.quantity -= 1
+
+      if (item.quantity === 0) {
+        this.$emit('removeFromCart', cart)
+      }
+
+      this.updateCart()
+    },
+
+    incrementQuantity(item) {
+      item.quantity += 1
+
+      this.updateCart()
+    },
+
+    updateCart() {
+      localStorage.setItem('cart', JSON.stringify(this.$store.state.cart))
+    },
+
+    removeFromCart(item) {
+      this.$emit('removeFromCart', item)
+
+      this.updateCart()
+    }
   },
 }
 </script>
--- ecommerce_front/src/views/Cart.vue
+++ ecommerce_front/src/views/Cart.vue
@@ -25,6 +25,7 @@
               v-for="item in cart.items"
               v-bind:key="item.product.id"
               v-bind:initialItem="item"
+              v-on:removeFromCart="removeFromCart"
           />
           </tbody>
         </table>
@@ -64,6 +65,12 @@ export default {
 
   mounted() {
     this.cart = this.$store.state.cart
+  },
+
+  methods: {
+    removeFromCart(item) {
+      this.cart.items = this.cart.items.filter(i => i.product.id !== item.product.id)
+    }
   },
 
   computed: {
