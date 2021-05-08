a868cd268b1bc8dc9be239b549564c6df616c916
chris <christopher.poncelet51@gmail.com>
2021-05-08 02:12:18 +0200

    login

--- ecommerce_front/src/views/Login.vue
+++ ecommerce_front/src/views/Login.vue
@@ -66,6 +66,46 @@ export default {
 
   mounted() {
     document.title = 'Login | Eshop'
+  },
+
+  methods: {
+    async submitForm() {
+      axios.defaults.headers.common["Authorization"] = ""
+
+      localStorage.removeItem("token")
+
+      const formData = {
+        username: this.username,
+        password: this.password
+      }
+
+      await axios
+          .post("api/v1/token/login/", formData)
+          .then(response => {
+            const token = response.data.auth_token
+
+            this.$store.commit('setToken', token)
+
+            axios.defaults.headers.common["Authorization"] = "Token " + token
+
+            localStorage.setItem("token", token)
+
+            const toPath = this.$route.query.to || '/cart'
+
+            this.$router.push(toPath)
+          })
+          .catch(error => {
+            if (error.response) {
+              for (const property in error.response.data) {
+                this.errors.push(`${property}: ${error.response.data[property]}`)
+              }
+            } else {
+              this.errors.push('Une erreur est survenue, veuillez réessayer')
+
+              console.log(JSON.stringify(error))
+            }
+          })
+    }
   }
 
 }
